bda9a759d41120f8a2a1ec8aa5a3c9769586731e
Jared Dantis <jareddantis@gmail.com>
2019-04-07 13:28:51 +0800

    lightdeck: Implement encoder rotation/press functionality

--- src/Lightdeck.ino
+++ src/Lightdeck.ino
@@ -10,6 +10,7 @@
 #include <LdConst.h>
 #include <LdMidi.h>
 #include <LdBtn.h>
+#include <Rotary.h>
 
 /**
  * Objects
@@ -24,13 +25,40 @@ LdBtn nextAdjBtn(PIN_BTN_ADJUP); ///< Adjustment selection button 1
 LdBtn prevAdjBtn(PIN_BTN_ADJDN); ///< Adjustment selection button 2
 LdBtn undoBtn(PIN_BTN_UNDO);     ///< Undo adjustment button
 
+/**
+ * Rotary encoder
+ */
+LdBtn encReset(PIN_ENC_SW);      ///< Click button
+Rotary* encoder;
+
 void setup() {
     mid.begin();
+
+    // Init encoder
+    encoder = new Rotary(PIN_ENC_CLK, PIN_ENC_DT);
+    attachInterrupt(0, onEncoderRotate, CHANGE);
+    attachInterrupt(1, onEncoderRotate, CHANGE);
 }
 
 void loop() {
     // Buttons
+    if (encReset.pressed()) onEncoderReset();
     if (prevAdjBtn.pressed()) adj.prevAdj();
     if (nextAdjBtn.pressed()) adj.nextAdj();
     if (undoBtn.pressed()) mid.sendNote(MID_CHAN_UNDO);
+}
+
+void onEncoderRotate() {
+    int num = adj.getNum();
+    unsigned char result = encoder -> process();
+
+    if (result == DIR_CW)
+        mid.sendAdjustment(num, 127);  // Increment on clockwise
+    else if (result == DIR_CCW)
+        mid.sendAdjustment(num, 1);    // Decrement on counterclockwise
+}
+
+void onEncoderReset() {
+    int num = adj.getNum();
+    mid.sendNote(num + 2);
 }
